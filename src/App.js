49f7e89e70e3a0be2335b741f3cd4554f718f394
Myles Nichols <mylesnichols21@yahoo.com>
2023-03-20 15:40:24 -0400

    changed file structure and added About.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import navbar from '/.components/navbar';
+import footer from "./components/footer";
+import About from './pages/About'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import ProfPic from "../img/myles-and-moon"
+
+// Adds about section
+const About = () => {
+    return (
+        <div className = "about">
+            <h2>About Me</h2>
+            <img src={ProfPic} alt= "Myles and his dog Moon" />
+            <p>
+                    Myles Nichols is a full-stack web developer based in centeral Illinois. I am constantly trying to learn new skills and 
+                improve my abilities in all aspects of my development journey. I am an excellent team player and really enjoy being part
+                of a well-oiled machine. The main reason that I became a web developer is to be apart of something that can affect peoples
+                lives in a positive way. A few of my projects can be fount in the Projects tab of my portfolio.
+                If you have any questions or comments concerning myself or my work, you may reach out to me in the "Contact" section.
+            </p>
+        </div>
+    );
+}
+
+export default About;
